733e7c721510ba46f5a2fb9282f8a628a27ff5d3
leeyangyangy <1787294587@qq.com>
2024-08-11 14:16:18 +0800

    init qt music project

---
+++ music.cpp
@@ -0,0 +1,19 @@
+#include "music.h"
+
+#include <QCoreApplication>
+#include <QFileInfoList>
+#include <QDir>
+#include <QProcess>
+
+Music::Music(QWidget *parent)
+    : QMainWindow(parent)
+{
+
+
+
+}
+
+Music::~Music()
+{
+}
+
---
+++ music.h
@@ -0,0 +1,24 @@
+#ifndef MUSIC_H
+#define MUSIC_H
+
+#include <QMainWindow>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include <QPushButton>
+#include <QSlider>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QListWidget>
+#include <QLabel>
+#include <QSpacerItem>
+#include <QDebug>
+
+class Music : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Music(QWidget *parent = nullptr);
+    ~Music();
+};
+#endif // MUSIC_H
